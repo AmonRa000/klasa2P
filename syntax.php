ec6079f00b68e96add9b54279c569033c4412459
AmonRa000 <borowyborowy4@gmail.com>
2024-09-13 15:37:13 +0200

    aa

---
+++ syntax.php
@@ -0,0 +1,7 @@
+<?php
+echo "funkcja echo dziala prawidlowo <br>" ;
+echo ("funkcja echo dziala prawidlowo <br>" ;
+echo "funkcja echo dziala prawidlowo <br>" ;
+echo "funkcja echo dziala prawidlowo <br>" ;
+
+?>
