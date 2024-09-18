e6e842669dea32118921b6ad5fab80cfabc0ef6c
AmonRa000 <borowyborowy4@gmail.com>
2024-09-18 19:18:42 +0200

    syntax.php

--- syntax.php
+++ syntax.php
@@ -1,7 +1,18 @@
 <?php
+#zadanie 1 
 echo 'funkcja echo dziala prawidlowo <br>' ;
 echo "funkcja echo dziala prawidlowo <br>" ;
 echo ('funkcja echo dziala prawidlowo <br>') ;
 echo ("funkcja echo dziala prawidlowo <br>") ;
+echo "funkcja "."echo "."dziala "."prawidlowo <br>";
+echo 'funkcja '.'echo '.'dziala '.'prawidlowo <br>';
+echo ("funkcja "."echo "."dziala "."prawidlowo <br>");
+echo ('funkcja '.'echo '.'dziala '.'prawidlowo <br>');
+$wiadomosc = "funkcja echo dziala prawidlowo <br>";
+echo $wiadomosc;
+$wiadomosc1 = "funkcja echo dziala prawidlowo <br>";
+echo ($wiadomosc1);
+
+
 
 ?>
