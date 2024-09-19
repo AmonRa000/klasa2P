295a1d13b5c89ee876f0342ab09b4c54a085c276
AmonRa000 <borowyborowy4@gmail.com>
2024-09-19 15:26:58 +0200

    lekcja

---
+++ zadanie1.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP zadanaie 1</title>
+</head>
+<body>
+<?php
+#1. 
+    echo 'funkcja echo dziala prawidlowo <br>' ;
+    echo "funkcja echo dziala prawidlowo <br>" ;
+    echo ('funkcja echo dziala prawidlowo <br>') ;
+    echo ("funkcja echo dziala prawidlowo <br>") ;
+    echo "funkcja "."echo "."dziala "."prawidlowo <br>";
+    echo 'funkcja '.'echo '.'dziala '.'prawidlowo <br>';
+    echo ("funkcja "."echo "."dziala "."prawidlowo <br>");
+    echo ('funkcja '.'echo '.'dziala '.'prawidlowo <br>');
+    $wiadomosc = "funkcja echo dziala prawidlowo <br>";
+    echo $wiadomosc;
+    $wiadomosc1 = "funkcja echo dziala prawidlowo <br>";
+    echo ($wiadomosc1);
+#2. 
+    print ("Zglasza "."sie funkcja print <br>");
+    print "Zglasza "."sie funkcja print <br>";
+#3. 
+    ?>
+    <?= "raz "."dwa <br>"; ?>
+    <?= "trzy "."cztery <br>";?>
+<?=
+#4.
+#komentarz jednoliniowy
+//drugi komentarz jednoliniowy
+/*
+komentarz wieloliniowy
+*/
+
+?>
+</body>
+</html>
