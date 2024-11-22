a66975b0992eb6ec3a4bbed71da687a7c0914c61
AmonRa000 <borowyborowy4@gmail.com>
2024-11-22 15:39:23 +0100

    piątek

---
+++ MasaGestosc/MasaGestosc.php
@@ -0,0 +1,18 @@
+<?php
+    class MasaGestosc{
+        private ?float $promien;
+        private ?string $substancja;
+        private ?float $gestosc;
+
+        public function __construct(floaat){
+
+        }
+
+        private function objetosc(): float{
+
+        }
+        public function masa(): float{
+
+        }
+    }
+?>
---
+++ MasaGestosc/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Obliczanie masy i gęstości</title>
+</head>
+<body>
+    <ol type="I">
+        <li>
+
+        </li>
+        <li>
+
+        </li>
+        <li>
+            
+        </li>
+    </ol>
+</body>
+</html>
