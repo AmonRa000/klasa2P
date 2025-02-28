f19b5b42898fbfd7fe68282b31e26dd905d84758
AmonRa000 <borowyborowy4@gmail.com>
2025-02-28 18:02:06 +0100

    json

--- ip_json.php
+++ ip_json.php
@@ -1,21 +1,34 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST" && !empty($_POST["adres"])) {
+    $ip = trim($_POST["adres"]);
+    $url = "http://ip-api.com/json/" .$ip;
+    
+    $json = file_get_contents($url);
+    $dane = json_decode($json, true); 
+}
+?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pl">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>IP_json</title>
 </head>
 <body>
-    <p>Sprawdź przybliżoną lokalizację adresu IP v.4</p>
-    <form method="post">
-        <a>http://ip=api.com/json/</a>
-        <label for=""></label>
-        <input type="text" name="IP">
+    <form method="POST">
+        <p>Sprawdź przybliżoną lokalizacje adresu IP v.4</p>
+        <label for="adres">http://ip-api.com/json/</label>
+        <input type="text" id="adres" name="adres" required>
         <input type="submit" value="Sprawdź IP">
     </form>
-    <?php
-    file_get_contents("http://ip=api.com/json/",$_POST['IP'] );
-    ?>
+    
+    <?php if (isset($json)): ?>
+        <p>Zwrócone dane (JSON):</p>
+        <pre><?= htmlspecialchars($json); ?></pre>
+        
+        <p>Zwrócone dane (array)</p>
+        <pre><?= print_r($dane); ?></pre>
+    <?php endif; ?>
 </body>
 </html>
