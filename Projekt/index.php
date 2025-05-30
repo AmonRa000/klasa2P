8118eff64d926287bb405b5479a64afc06bdb5ec
AmonRa000 <borowyborowy4@gmail.com>
2025-05-30 22:51:24 +0200

    dokończone

--- Projekt/index.php
+++ Projekt/index.php
@@ -1,8 +1,16 @@
 <?php
-    $aktywny=(isset($_GET['Page']) && $_GET['Page'] == "czytelnicy") ? 'aktywny':'';
+    $aktywny1=(isset($_GET['page']) && $_GET['page'] == "glowna") ? 'aktywny1':'' ;
+    $aktywny2=(isset($_GET['page']) && $_GET['page'] == "czytelnicy") ? 'aktywny2':'' ;
+    $aktywny3=(isset($_GET['page']) && $_GET['page'] == "dzialy") ? 'aktywny3':'' ;
+    $aktywny4=(isset($_GET['page']) && $_GET['page'] == "dzialy2") ? 'aktywny4':'' ;
+    $aktywny5=(isset($_GET['page']) && $_GET['page'] == "ksiazki") ? 'aktywny5':'' ;
+    $aktywny6=(isset($_GET['page']) && $_GET['page'] == "pracownicy") ? 'aktywny6':'' ;
+    $aktywny7=(isset($_GET['page']) && $_GET['page'] == "stanowiska") ? 'aktywny7':'' ;
+    $aktywny8=(isset($_GET['page']) && $_GET['page'] == "wypozyczalnia") ? 'aktywny8':'' ;
+
 ?>
 <!DOCTYPE html>
-<html lang="ppl">
+<html lang="pl">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -24,14 +32,14 @@
 <body>
     <div class="row">
     <div class="column left">
-        <a href="index.php?page=glowna">Strona główna</a>
-        <a href="index.php?page=czytelnicy" class="<?=$aktywny?>">Tabela czytelnicy</a>
-        <a href="index.php?page=dzialy">Tabela działy</a>
-        <a href="index.php?page=dzialy2">Tabela działy2</a>
-        <a href="index.php?page=ksiazki">Tabela książki</a>
-        <a href="index.php?page=pracownicy">Tabela pracownicy</a>
-        <a href="index.php?page=stanowiska">Tabela stanowiska</a>
-        <a href="index.php?page=wypozyczalnia">Tabela wypożyczenia</a>   
+        <a href="index.php?page=glowna" class="<?=$aktywny1?>">Strona główna</a>
+        <a href="index.php?page=czytelnicy" class="<?=$aktywny2?>">Tabela czytelnicy</a>
+        <a href="index.php?page=dzialy" class="<?=$aktywny3?>">Tabela działy</a>
+        <a href="index.php?page=dzialy2" class="<?=$aktywny4?>">Tabela działy2</a>
+        <a href="index.php?page=ksiazki" class="<?=$aktywny5?>">Tabela książki</a>
+        <a href="index.php?page=pracownicy" class="<?=$aktywny6?>">Tabela pracownicy</a>
+        <a href="index.php?page=stanowiska" class="<?=$aktywny7?>">Tabela stanowiska</a>
+        <a href="index.php?page=wypozyczalnia" class="<?=$aktywny8?>">Tabela wypożyczenia</a>   
     </div>
     <div class="column right">
         <?php
